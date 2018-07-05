7b05ac2a25b14794c281eb45ee5abf0e536e05a7
WeekFax <we9697@mail.ru>
2018-07-05 11:19:01 +0700

    v0.2

--- source.py
+++ source.py
@@ -2,27 +2,11 @@ import numpy as np
 import imutils
 import cv2
 
+file_name="1.jpg"
 
-def get_skin_mask(img):
-    """
-    Получаем маску кожи
-    :param img: Данное изображение
-    :return:    Возвращает маску с участками кожи
-    """
-    lower = np.array([0, 48, 80], dtype = "uint8")  # Нижний предел цвета кожи
-    upper = np.array([20, 255, 255], dtype = "uint8")   # Верхний предел цвета кожи
-    converted = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)  # Преобразовываем данное изображение к цветовой схеме HSV
-    skinMask = cv2.inRange(converted, lower, upper) # Создаем маску с местами с цветами, между верхним и нижним пределами
+cascadePath = "haarcascade_frontalface_default.xml"
+faceCascade = cv2.CascadeClassifier(cascadePath)
 
-
-    kernel1 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
-    skinMask = cv2.erode(skinMask, kernel1, iterations=2)
-    skinMask = cv2.dilate(skinMask, kernel1, iterations=2)
-
-    kernel2 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (23, 23))
-    skinMask = cv2.morphologyEx(skinMask, cv2.MORPH_OPEN, kernel2)
-    skinMask = cv2.GaussianBlur(skinMask, (3,3), 0)
-    return skinMask
 
 def get_head_mask(img):
     """
@@ -31,54 +15,76 @@ def get_head_mask(img):
     :param img: Данное изображение
     :return:    Возвращаетм маску с вырезанным БГ
     """
-
-    mask = np.zeros(img.shape[:2], np.uint8)    # Создаем пустую маску
-    h, w = img.shape[:2]
+    mask = np.zeros(img.shape[:2], np.uint8)
     bgdModel = np.zeros((1, 65), np.float64)
     fgdModel = np.zeros((1, 65), np.float64)
-    rect = (20, 20, w - 40, h - 40)
-    cv2.grabCut(img, mask, rect, bgdModel, fgdModel, 5, cv2.GC_INIT_WITH_RECT)  # Создаем маски с БГ и ФГ
-    mask2 = np.where((mask == 2) | (mask == 0), 0, 1).astype('uint8')   # Забираем маску с БГ
+    faces = faceCascade.detectMultiScale(img, scaleFactor=1.1, minNeighbors=5, minSize=(50, 50))    #Находим лица
+    if len(faces) != 0:
+        x, y, w, h = faces[0]
+        (x, y, w, h) = (x - 40, y - 100, w + 80, h + 200)
+        rect1 = (x, y, w, h)
+        cv2.grabCut(img, mask, rect1, bgdModel, fgdModel, 5, cv2.GC_INIT_WITH_RECT)     #Обрезаем БГ вокруг головы
+    mask2 = np.where((mask == 2) | (mask == 0), 0, 1).astype('uint8')  # Забираем маску с БГ
+
     return mask2
 
-def get_hair_mask(img):
+def is_bold(pnt, hair_mask):
     """
-    Получаем маску волос
-    :param img: Данное изображение
-    :return: Возвращает маску волос
+    Проверяем лысая ли голова
+    :param pnt: Верхняя точка головы
+    :param hair_mask: Маска с волосами
+    :return: True если лысый, иначе False
     """
-    mask1 = get_head_mask(frame)    # Получаем маску головы
-    mask2 = cv2.bitwise_not(get_skin_mask(frame))    # Получаем маску с кожей и инвертируем её
+    roi = hair_mask[pnt[1]:pnt[1] + 40, pnt[0] - 40:pnt[0] + 40]    # Выделяем прямоугольник под верхней точкой
+    cnt = cv2.countNonZero(roi) # Считаем кол-во ненулевых точек в этом прямоугольнике
+    # Если кол-во точек меньше 25%, то считаем, что голова лысая
+    if cnt < 800:
+        print("Bold")
+        return True
+    else:
+        print("Not Bold")
+        return False
 
-    mask = cv2.bitwise_and(mask1, mask2)    # Создаем маску из общего двух масок
 
-    kernel2 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (15, 15))
-    mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel2)  # Удаляем лишние пятна в маске
-    return mask
+img1 = cv2.imread(file_name)     # Загружаем изображение
+img1 = imutils.resize(img1, height=500)     # Приводим к 500px по высоте
+mask = get_head_mask(img1)      # Получаем маску головы (без БГ)
+
+# Находим контуры, берем самый большой и запомиаем его верхнюю точку как верхушку головы
+cnts = cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)[1]
+cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
+cnt=cnts[0]
+topmost = tuple(cnt[cnt[:,:,1].argmin()][0])
 
 
-frame=cv2.imread("1.jpg")
+# Убираем лицо по цвету кожи
+lower = np.array([0, 0, 100], dtype="uint8")  # Нижний предел цвета кожи
+upper = np.array([255, 255, 255], dtype="uint8")  # Верхний предел цвета кожи
+converted = cv2.cvtColor(img1, cv2.COLOR_BGR2HSV)   # Переводим в цветовой формат HSV
+skinMask = cv2.inRange(converted, lower, upper)     # Записываем маску из мест, где цвет находся между пределами
+mask[skinMask == 255] = 0   # Убираем из маски головы маску лица
 
-frame = imutils.resize(frame, height=500)   # Задаем размеры изобрадения (фильтры настроены под это размер
+kernel1 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+mask = cv2.dilate(mask, kernel1, iterations=1)
+i1 = cv2.bitwise_and(img1, img1, mask=mask)
 
-hair_mask=get_hair_mask(frame)
-hair=cv2.bitwise_and(frame, frame, mask=hair_mask)  # Вырезаем волосы по маске
+# Если голова лысая, то выводим, что лысый и выводим координаты верхней точки головы
+if is_bold(topmost,mask):
+    cv2.rectangle(img1,topmost,topmost,(0,0,255),5)
+    print(topmost)
 
-# Находим контуры на маске
-cnts = cv2.findContours(hair_mask.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+# Иначе пишем, что не лысый и выводи координаты самого большого контура
+else:
+    cnts = cv2.findContours(mask.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[1]
+    cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
+    cv2.drawContours(img1,[cnts[0]],-1,(0,0,255),2)
+    for c in cnts[0]:
+        print(c)
 
-# Выводим координаты контура
-for c in cnts:
-    for b in c:
-        print(b)
-    #cv2.drawContours(frame, [c], -1, (255, 0, 0), 1)
-
-# Объединяем изображения до и после
-frame=np.hstack([frame, hair])
-frame = imutils.resize(frame, height=500)
-
+# Выводим изображение в цикле
 while True:
-    cv2.imshow("images",frame)
-    if cv2.waitKey(5)==27:
+    cv2.imshow("image1", img1)
+    # Выход на Esc
+    if cv2.waitKey(5) == 27:
         break
+
